2df187dd5075f3fc5bc8a3bae89cb153485d97a6
ayan-82 <53819933+ayan-82@users.noreply.github.com>
2019-08-07 12:35:13 +0800

    Update AmyYan_Q2.py

--- L1/AmyYan_Q2.py
+++ L1/AmyYan_Q2.py
@@ -4,11 +4,10 @@ print (5 + 30*20)
 print((5+30) * 20)
 print (1+(2+20*3)/(4*2))
 print (1+2**10)
-print (ceil(29/4))
+print (ceil(29/4), "taxis are needed.")
 r = sqrt(100/pi)
 A = (r*2+1)**2
 print (A)
-a = "2"
-print (2.0/0)
+
 
 
