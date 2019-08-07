680644bd87c002131e012e083d8e5ff4f301c87f
jacky0313 <53846192+jacky0313@users.noreply.github.com>
2019-08-07 12:07:46 +0800

    Add files via upload

---
+++ L1/Jackyshen_Q4.py
@@ -0,0 +1,14 @@
+year = input('Year')
+year = int(year)
+if year%4 > 0:
+    ans = 0
+if year%4 == 0:
+    if year%100 > 0 :
+        ans = 1
+    if year%100 == 0 :
+        ans = 0
+if ans == 1 :
+    print('Yes, it is a leap year')
+if ans == 0 :
+    print("No, it is not a leap year")
+    
