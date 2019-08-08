b69f68513b3c2d9ffedbd10c8c17bb68ac543324
ayan-82 <53819933+ayan-82@users.noreply.github.com>
2019-08-08 18:06:24 +0800

    Update and rename AmyYan_Q3.py to Q3.py

--- L1/Q3.py
+++ L1/Q3.py
@@ -2,5 +2,5 @@ from math import *
 r = input("radius of circle:\n")
 A = int(r)**2*pi
 print("The area of circle is" , round(A,2))
-print("The area of circle is%10.2f" % A)
+print("The area of circle is %.2f" % A)
 
