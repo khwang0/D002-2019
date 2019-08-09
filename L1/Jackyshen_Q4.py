c593def9725ec58037047089eafc7af5dc6ec646
jacky0313 <53846192+jacky0313@users.noreply.github.com>
2019-08-09 09:59:57 +0800

    new

--- L1/Jackyshen_Q4.py
+++ L1/Jackyshen_Q4.py
@@ -6,7 +6,10 @@ if year%4 == 0:
     if year%100 > 0 :
         ans = 1
     if year%100 == 0 :
-        ans = 0
+        if year%400 >0 :
+            ans = 0
+        if year%400 == 0:
+            ans = 1
 if ans == 1 :
     print('Yes, it is a leap year')
 if ans == 0 :
