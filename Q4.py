c8babde17e27be238d97d880c9684721cade070a
adrianlopa <53845614+adrianlopa@users.noreply.github.com>
2019-08-09 10:11:49 +0800

    Update Q4.py

--- Q4.py
+++ Q4.py
@@ -1,7 +1,5 @@
 x=int(input('enter a year'))
-if x%4 == 0 and x%400 == 0:
-    print ("Yes, it is a leap year")
-elif x%100==0:
-    print ("No, it is not a leap year")
+if x%4 == 0 or x%400 == 0 or x%100:
+print ("Yes, it is a leap year")
 else:
     print ("No, it is not a leap year")
