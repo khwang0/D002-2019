829def49a611a0d3c657f65bea42ad62d096708d
ivanchau1001 <53845593+ivanchau1001@users.noreply.github.com>
2019-08-11 15:11:55 +0800

    Q3 upload

---
+++ L2/Q3_ivanchau.py
@@ -0,0 +1,9 @@
+for n in [-120, 14, 93, 320, 1, -999]:
+    print(n)
+max = 120
+for n in [14, 93, 320, 1, -999]:
+    while n > max:
+        if n > max:
+            max = n
+            print(max)
+        
