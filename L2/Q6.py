9297d0b6c8346711d917c02074f5f07e9b6743df
ayan-82 <53819933+ayan-82@users.noreply.github.com>
2019-08-11 22:24:38 +0800

    Add files via upload

--- L2/Q6.py
+++ L2/Q6.py
@@ -1,20 +1,31 @@
 #L2 Q6: Banana Guessing game
 
+
 #Step 1: Import necessary modules
 import random
 #Step 2: Welcome Message
 print('''Welcome to the Banana Guessing Game
 Dave hid some bananas. Your task is to find out the number of bananas he hid.''')
+
+
 #Step 3: Choose a random number between 1-100
 n = random.randint(1,100)
+
 print ("shhh, Dave hides %s bananas" % n)
 # define a flag for found/not found and counter on how many trials
 found = False
 count = 0
 #Step 4: Give three chances to the players 
 
-
-
+while (count < 3 and found == False):
+    p = int(input("Guess a number...\n"))
+    count = count +1
+    if p == n:
+        found = True
+    else:
+        found = False
+        print("Try again")
+        
 #Step 5: Display a message
 if found == True:
         print('You got the correct guess in %d trials' % count)
