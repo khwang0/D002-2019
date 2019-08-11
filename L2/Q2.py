1a50adc0282c34e0a55398a6860af938079a6930
ayan-82 <53819933+ayan-82@users.noreply.github.com>
2019-08-11 14:50:27 +0800

    Add files via upload

---
+++ L2/Q2.py
@@ -0,0 +1,14 @@
+'''
+Please input an positive integer number:
+
+3 is a prime
+4 is even continue
+5 is a prime
+6 is even continue
+7 is a prime
+8 is even continue
+9 has factor 3; break
+10 is even continue
+
+'''
+
