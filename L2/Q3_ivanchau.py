70c2066f97f386cc16ba42848d9f796765eb5bea
ivanchau1001 <53845593+ivanchau1001@users.noreply.github.com>
2019-08-12 10:12:50 +0800

    Delete Q3_ivanchau.py

--- L2/Q3_ivanchau.py
+++
@@ -1,9 +0,0 @@
-for n in [-120, 14, 93, 320, 1, -999]:
-    print(n)
-max = 120
-for n in [14, 93, 320, 1, -999]:
-    while n > max:
-        if n > max:
-            max = n
-            print(max)
-        
