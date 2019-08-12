1c2732fde4aa5f2e84b445cde0bc6837d2a75ba7
ayan-82 <53819933+ayan-82@users.noreply.github.com>
2019-08-12 11:59:01 +0800

    Add files via upload

--- L2/Q4.py
+++ L2/Q4.py
@@ -1,28 +1,22 @@
+'''a = 0
+max = 0
+while a <= 10:
+    b = 10 - a
+    if a*b > max:
+        max = a*b
+    a = a+ 1
 
-sum = int(input("a+b+c+d = ?\n") )
-a = int(sum/4)
-num = [a,a,a,a]
-mmp = 0
+print("The max value is %d" % max
+'''
+sum = int(input("a+b+c+d + ?"))
+a = 0
+max = 0
+for a in range (0,(sum+1)):
+    for b in range (0, (sum+1)-a):
+        c = sum-a-b
+        value = a*b + b*c
+        if value > max:
+            max=value
+            print(a, b, c, d, max)
 
-print("Possible combinations:")
-for x in range (0,3):
-      y = 15 + x*14 
-      for num[x] in range (y, 0, -1):
-
-        arr = list(num)
-        arr.sort()
-
-        mp = arr[0]*arr[2]+arr[2]*arr[3]+arr[3]*arr[1]
-
-        if mp> mmp:
-           mmp = mp
-           v = list(arr)
-
-        print("a=%d b=%d c=%d d=%d" % (arr[0], arr[2], arr[3], arr[1]))
-        num[x+1] = num[x+1] +1
-        
-print("Maximum value of ab+bc+cd =", mmp)
-print("a=%d b=%d c=%d d=%d" % (v[0], v[2], v[3], v[1]))
-
-
-
+print (max)
