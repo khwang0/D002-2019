92566c2502da28d5f10c0940288cdf5ceb1fe3c4
MegaWalls <taisiuhang@yahoo.com.hk>
2019-08-12 10:03:20 +0800

    Now working

--- L2/L2Q4.py
+++ L2/L2Q4.py
@@ -5,35 +5,40 @@ b = 0
 c = 0
 d = 60
 while a < 60:
-    a = a + 1
     while b <= 60 - a - c:
-        b = b + 1
-        while c <= 60 - a - b:
+        while c <= 60 - a - b -d:
             c = c + 1
             d = 60 - a - b - c
             if d <= 0:
-                c=0
-                d=0
                 break
             if maxvalue < a*b + b*c + c*d:
                 maxvalue = a*b + b*c + c*d
-                print(a,b,c,d)
-        if d <=0:
-            b=0
-            c=0
-            d=0
-            break
-        d = 60 - a - b - c
-        if maxvalue < a*b + b*c + c*d:
-            maxvalue = a*b + b*c + c*d
             print(a,b,c,d)
-    if d <=0:
-        b=0
+            d = 0
+        b = b + 1
         c=0
         d=0
-        break
-    d = 60 - a - b - c
-    if maxvalue < a*b + b*c + c*d:
-        maxvalue = a*b + b*c + c*d
-        print(a,b,c,d)
+    a = a + 1
+    b=0
+    c=0
+    d=0
+        #d = 60 - a - b - c
+        #if d<= 0:
+            #break
+        #if maxvalue < a*b + b*c + c*d:
+            #maxvalue = a*b + b*c + c*d
+        #print(a,b,c,d)
+        #d=0
+
+    #d = 60 - a - b - c
+    #if d <= 0:
+        #break
+    #if maxvalue < a*b + b*c + c*d:
+        #maxvalue = a*b + b*c + c*d
+    #print(a,b,c,d)
+    #d=0
 print(maxvalue)
+
+
+
+
