740d79d561237531d229c109613254b63b1611af
edwardzhu13 <53845691+edwardzhu13@users.noreply.github.com>
2019-08-13 19:58:49 +0800

    Add files via upload

---
+++ L3/Q6.py
@@ -0,0 +1,20 @@
+import random
+print('''Welcome to the Banana Guessing Game
+Dave hid some bananas. Your task is to find out the number of bananas he hid.\nWhat is your guess\n''')
+n = random.randint(8,10)
+found = False
+count = 1
+
+while found == True or count < 4:
+    if n == int(input()):
+        found == True
+        print('You got the correct guess in %d trials' % count)
+        print('Dave\'s banana are now all yours!')
+        break    
+    else:
+        count = count + 1
+        print("Try again!")
+    
+if n != int(input()) and count >= 3:
+    print("You failed to find the number of bananas Dave hid!")
+    print("He had", n, "bananas.")
