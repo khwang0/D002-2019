46c957efaf178bbe2757bb259f100ea4c86eb95a
ayan-82 <53819933+ayan-82@users.noreply.github.com>
2019-08-14 19:22:54 +0800

    Add files via upload

---
+++ L4/Bonus.py
@@ -0,0 +1,17 @@
+def is_over(a1,a2,c1,c2):
+
+
+  if (a1[0] > c2[0] or a2[0] > c1[0]):
+    return True
+
+  if (a1[1] < c2[1] or a2[1] > c1[1]):
+    return True
+
+  return False
+
+
+
+  
+
+  
+
