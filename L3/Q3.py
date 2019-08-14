a36792dd549f8deb2d82acf1c353537339edc5bb
ayan-82 <53819933+ayan-82@users.noreply.github.com>
2019-08-14 10:06:37 +0800

    Update Q3.py

--- L3/Q3.py
+++ L3/Q3.py
@@ -1,7 +1,7 @@
 vowels = []
 for i in range (0,10):
     w = input("Please input word%d:\n"%(i+1))
-    if w[0] == "A" or w[0] == "E" or w[0] == "I" or  w[0] == "O" or w[0] == "U":
+    if w[0] in "AEIOUaeiou":
         vowels.append(w)
 
 print (vowels)
