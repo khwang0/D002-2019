e3a6d3d781b3cbd713cb0dd046d64efd559535ba
ivanchau1001 <53845593+ivanchau1001@users.noreply.github.com>
2019-08-14 10:07:39 +0800

    Update Q3_ivanchau.py

--- L3/Q3_ivanchau.py
+++ L3/Q3_ivanchau.py
@@ -2,7 +2,7 @@ list = []
 for i in range(10):
     word = input("Enter 10 words\n")
     print(word[0])
-    if word[0]== "a" or word[0]== "e" or word[0] == "i" or word[0] == "o" or word[0] == "u"or word[0]== "A" or word[0] == "E" or word[0] == "I" or word[0] == "O"or word[0] == "U":
+    if word[0]in "a, e, i, o, u, A, E, I, O, U" :
         list.append(word)
         print(list)
     
