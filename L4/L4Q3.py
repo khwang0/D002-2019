fe74ad0e852513ed7e138b83298158e911857fe9
MegaWalls <taisiuhang@yahoo.com.hk>
2019-08-14 10:40:21 +0800

    Add files via upload

---
+++ L4/L4Q3.py
@@ -0,0 +1,11 @@
+def factors(n): # your code here
+    result = []
+    for x in range(1, n + 1):
+        if n % x == 0:
+            result = result + [x]
+    return result
+    
+print(factors(40))  # prints [1, 2, 4, 5, 8, 10, 20, 40]
+a = factors(30)
+if 5 in a:
+     print("5 is a factor of 30") # it prints!
